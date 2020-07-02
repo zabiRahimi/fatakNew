ec156dde83ce7111312c0622b87eecc4231fc10b
zabiRahimi <rahimi.z1360@gmail.com>
2020-07-02 17:04:12 +0430

    update5

--- resources/js/components/nav/navMo.js
+++ resources/js/components/nav/navMo.js
@@ -4,27 +4,26 @@ import $ from "jquery";
 import { Link } from "react-router-dom";
 // #5f6f81
 class NavMo extends Component {
-    showUlSub = (e) => {
-        // document.getElementsByClassName('menuMobailUlSub').style.color = "blue";
-        // var search_span = document.getElementsByClassName("menuMobailUlSub");
-        // search_span[0].style.display = "flex";
-        var getId='#'+e.currentTarget.getAttribute('id') + ' .menuMobailUlSub ';
-        var getsortUp='#'+e.currentTarget.getAttribute('id') + ' .sortUp ';
-        var getsortDown='#'+e.currentTarget.getAttribute('id') + ' .sortDown ';
-        $(`.menuMobailUlSub:not(${getId})`).css({"display": "none"});
-        // $(getId).css("display", "flex");
-        $(getId).toggle('slow');
-        $(getsortUp).toggle('slow');
-        $(getsortDown).toggle('slow');
-       
-        //  var r =e.currentTarget.getAttribute('id')
-        // alert(getId)
-
+     showUlSub = (e) => {
+        const liId='#'+e.currentTarget.getAttribute('id');
+        const menuMobailUlSub=liId + ' .menuMobailUlSub ';
+        const sortUp=liId + ' .sortUp ';
+        const sortDown=liId + ' .sortDown ';
+        $(`.menuMobailUlSub:not(${menuMobailUlSub})`).css({"display": "none"});
+        $(menuMobailUlSub).toggle('slow',function(){
+             const subDisplay= $(this).css('display');
+             if(subDisplay ==='block'){
+                 $(liId).css({"background-color":  "#ebeff4"})
+             }
+             else{$(liId).css({"background-color":  "#f1f5fa"})}
+        });
+        $(sortUp).toggle('slow');
+        $(sortDown).toggle('slow');
     }
     openOrExitMenu=()=>{
-        $('.menuMobailOpacity').toggle('slow',function(e){
-             const u= $('.menuMobailOpacity').css('display');
-             if(u ==='block'){
+        $('.menuMobailOpacity').toggle('slow',function(){
+             const menuMobailOpacity= $('.menuMobailOpacity').css('display');
+             if(menuMobailOpacity ==='block'){
                  $('body').css({"overflow": "hidden"})
              }
              else{$('body').css({"overflow": "visible"})}
@@ -39,9 +38,8 @@ class NavMo extends Component {
                 <div className="navMCT"> منو</div>
                 </div>
                 <div className="menuMobailOpacity" onClick={this.openOrExitMenu}></div>
-                
+
                 <div className="menuMobailMenu">
-                    
                     <div className="closeMenuMobail" >
                         <div className="closeMMRigth">
                             <div className="closeMMRI"><i className="fas fa-ellipsis-v"></i></div>
@@ -51,33 +49,31 @@ class NavMo extends Component {
                             <button onClick={this.openOrExitMenu}><span aria-hidden="true">&times;</span></button>
                         </div>
                     </div>
-
                     <ul className="menuMobailUl">
                         <li className="menuMULi">
                             <div className="">
                                 <i className="far fa-window-maximize"></i>
-                                <Link to="">خانه</Link>
+                                <Link to="/" onClick={this.openOrExitMenu}>خانه</Link>
                             </div>
                         </li>
                         <li className="menuMULiHasSub" id='orderLi' onClick={this.showUlSub}>
-                            
                             <div className="menuMULHSUp">
                                 <div className="menuMULHSURight">
                                     <i className="far fa-window-maximize"></i>
                                     <span>سفارش</span>
                                 </div>
                                 <div className="menuMULHSULeft">
-                                    <img className="sortDown" src="./images/imageWeb/sort1.png" />
+                                  <i className="fas fa-sort-down sortDown" ></i>
+                                  <i className="fas fa-sort-up sortUp" ></i>
                                 </div>
                             </div>
                             <ul className="menuMobailUlSub">
-                                <li><Link to="">ثبت سفارش</Link></li>
-                                <li><Link to="">پیگیری سفارش</Link></li>
-                                <li><Link to="">پیگیری خرید</Link></li>
-                                <li><Link to="">راهنما</Link></li>
+                                <li><Link to="" onClick={this.openOrExitMenu}>ثبت سفارش</Link></li>
+                                <li><Link to="" onClick={this.openOrExitMenu}>پیگیری سفارش</Link></li>
+                                <li><Link to="" onClick={this.openOrExitMenu}>پیگیری خرید</Link></li>
+                                <li><Link to="" onClick={this.openOrExitMenu}>راهنما</Link></li>
                             </ul>
                         </li>
-
                         <li className="menuMULiHasSub" id='chaneelLi' onClick={this.showUlSub}>
                             <div className="menuMULHSUp">
                                 <div className="menuMULHSURight">
@@ -85,19 +81,16 @@ class NavMo extends Component {
                                     <span>مدیریت کانال و گروه</span>
                                 </div>
                                 <div className="menuMULHSULeft">
-                                    <i className="fas fa-comments sortUp"></i>
-                                    <img className="sortDown" src="./images/imageWeb/sort1.png" />
-                                    
+                                  <i className="fas fa-sort-down sortDown" ></i>
+                                  <i className="fas fa-sort-up sortUp" ></i>
                                 </div>
                             </div>
-
                             <ul className="menuMobailUlSub">
-                                <li><Link to="">ورود</Link></li>
-                                <li><Link to="">ثبت نام</Link></li>
-                                <li><Link to="">راهنما</Link></li>
+                                <li><Link to=""onClick={this.openOrExitMenu}>ورود</Link></li>
+                                <li><Link to=""onClick={this.openOrExitMenu}>ثبت نام</Link></li>
+                                <li><Link to=""onClick={this.openOrExitMenu}> راهنما</Link></li>
                             </ul>
                         </li>
-
                         <li className="menuMULiHasSub" id='shopLi' onClick={this.showUlSub}>
                             <div className="menuMULHSUp">
                                 <div className="menuMULHSURight">
@@ -105,42 +98,38 @@ class NavMo extends Component {
                                     <span>مدیریت فروشندگان</span>
                                 </div>
                                 <div className="menuMULHSULeft">
-                                    <img className="sortDown" src="./images/imageWeb/sort1.png" />
+                                  <i className="fas fa-sort-down sortDown" ></i>
+                                  <i className="fas fa-sort-up sortUp" ></i>
                                 </div>
                             </div>
                             <ul className="menuMobailUlSub">
-                                <li><Link to="">ورود</Link></li>
-                                <li><Link to="">ثبت نام</Link></li>
-                                <li><Link to="">راهنما</Link></li>
+                                <li><Link to=""onClick={this.openOrExitMenu}>ورود</Link></li>
+                                <li><Link to=""onClick={this.openOrExitMenu}>ثبت نام</Link></li>
+                                <li><Link to=""onClick={this.openOrExitMenu}>راهنما</Link></li>
                             </ul>
                         </li>
-
                         <li className="menuMobailUl">
-
                             <div className="">
                                 <i className="fas fa-info-circle"></i>
-                                <Link to="">درباره ما</Link>
+                                <Link to=""onClick={this.openOrExitMenu}>درباره ما</Link>
                             </div>
                         </li>
-
                         <li className="menuMobailUl">
-                            
                             <div className="">
                                 <i className="fas fa-phone"></i>
-                                <Link to="">ارتباط با ما</Link>
+                                <Link to=""onClick={this.openOrExitMenu}>ارتباط با ما</Link>
                             </div>
                         </li>
                         <li className="menuMobailUl">
                             <div className="">
                                 <i className="fas fa-balance-scale"></i>
-                                <Link to="">قوانین و مقرارت</Link>
+                                <Link to=""onClick={this.openOrExitMenu}>قوانین و مقرارت</Link>
                             </div>
-
                         </li>
                         <li className="menuMobailUl">
                             <div className="">
                                 <i className="fas fa-gavel"></i>
-                                <Link to="">ثبت شکایت</Link>
+                                <Link to=""onClick={this.openOrExitMenu}>ثبت شکایت</Link>
                             </div>
                         </li>
                         <li className="menuMULiHasSub" id='instructLi' onClick={this.showUlSub}>
@@ -150,44 +139,20 @@ class NavMo extends Component {
                                     <span>راهنما</span>
                                 </div>
                                 <div className="menuMULHSULeft">
-                                    <img className="sortDown" src="./images/imageWeb/sort1.png" />
+                                  <i className="fas fa-sort-down sortDown" ></i>
+                                  <i className="fas fa-sort-up sortUp" ></i>
                                 </div>
                             </div>
-
                             <ul className="menuMobailUlSub">
-                                <li>راهنمای استفاده از سایت</li>
-                                <li>راهنمای خرید محصول</li>
-                                <li>راهنمای سفارش محصول جدید</li>
+                                <li><Link to=""onClick={this.openOrExitMenu}> راهنمای استفاده از سایت </Link></li>
+                                <li><Link to=""onClick={this.openOrExitMenu}> راهنمای خرید محصول </Link></li>
+                                <li><Link to=""onClick={this.openOrExitMenu}> راهنمای سفارش محصول جدید </Link></li>
                             </ul>
                         </li>
-
-                        {/* <a href="/"><li className="modal_hide"><span><i className="far fa-window-maximize"></i> صفحه اصلی</span> </li></a>
-                    <a href="/page_login"><li className="modal_hide" onclick="modal_sub_show()"><span><i className="fas fa-comments"></i> مدیریت کانال و گروه</span> </li></a>
-                    <a href="/pageloginShop"><li className="modal_hide" onclick="modal_sub_show()"><span><i className="fas fa-user-tag"></i> مدیریت فروشندگان </span> </li></a>
-                    <li className="modal_hide" onclick="modal_sub_show() ;hide_menu_mobail(); $('#modal_ghanon').modal('show')"><span><i className="fas fa-balance-scale"></i> قوانین و مقررات</span> </li>
-                    <li className="modal_hide" onclick="modal_sub_show(); hide_menu_mobail(); $('#modal_shekait').modal('show')" ><span><i className="fas fa-gavel"></i> شکایت</span> </li>
-                    <li className="modal_hide rahnama_m" onclick="modal_sub_show('rahnama_m')"><span><i className="fas fa-compass"></i> راهنما</span>
-                        <img src="../../img_site/sort1.png" />
-                        <ul>
-                            <li>راهنمای استفاده از سایت</li>
-                            <li>راهنمای خرید محصول</li>
-                            <li>راهنمای سفارش محصول جدید</li>
-                        </ul>
-                    </li>
-                    <li claclassNamess="modal_hide" onclick="modal_sub_show();hide_menu_mobail(); $('#modal_h_login').modal('show');captcha()"><span><i class="fas fa-sign-in-alt"></i> ورود</span> </li>
-                    <li className="modal_hide" onclick="modal_sub_show();hide_menu_mobail(); $('#modal_h_sabtname').modal('show');captcha()"><span><i className="fas fa-user-plus"></i> ثبت نام</span> </li>
-                    <li className="modal_hide" onclick="modal_sub_show();hide_menu_mobail(); $('#about_we').modal('show')"><span><i className="fas fa-info-circle"></i> درباره ما</span> </li>
-                    <li className="modal_hide" onclick="modal_sub_show();hide_menu_mobail(); $('#contact_we').modal('show')"><span><i className="fas fa-phone"></i> تماس با ما</span> </li> */}
                     </ul>
-
                 </div>
             </div>
         )
     }
 }
-// function showUlSub(){
-//     // document.getElementById('menuMULiHasSub').style.display ='flex';
-//     // document.getElementById("menuMULiHasSub").style.color = "blue";
-//     alert('kkk')
-// }
 export default NavMo;
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,32 +13,32 @@
     <title>fataknew</title>
 
     <!-- Scripts -->
-    
+
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="\css\main.css" rel="stylesheet">
-    <link href="\css\navMo.css" rel="stylesheet">
+    <!-- <link href="\css\navPc.css" rel="stylesheet"> -->
 </head>
 <body>
     <div>
             @yield('content')
-        
+
     </div>
     <script src="{{ asset('js/app.js') }}" defer></script>
     <script >
-        alert('kdkksdf')
+        // alert('kdkksdf')
         // var fixed=$('#navContiner').offset();
         // alert(fixed)
         // $(window).scroll(function() {
         //     //    const fixed =300;
         //     // console.log(600)
-            
+
         //         if ($(document).scrollTop() > fixed.top) {
         //           //   alert('mohamad')
         //           $('#navContiner').css({"width":"100%","top":"0","position": "fixed" , "z-index": "333333"});
