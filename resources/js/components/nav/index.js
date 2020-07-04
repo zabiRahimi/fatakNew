e2f155d1e80701cca41e1e61057651f8e1c76c3f
rza166 <esi191@yahoo.com>
2020-07-04 09:01:55 +0330

    update6

--- resources/js/components/nav/index.js
+++ resources/js/components/nav/index.js
@@ -22,8 +22,40 @@ class Nav extends Component {
     //     const man=$("#navContiner").offset();
     //     console.log(man)
     //    }
+   
     componentDidMount(){
-        // console.log(this.state.fixed)
+        window.addEventListener('load',()=>{
+            // alert(89)
+            var fixed=$('#navContiner').offset();
+            window.addEventListener("resize", ()=>{
+                fixed=$('#navContiner').offset();
+            });
+            window.addEventListener('scroll',()=>{
+               
+                    if ($(document).scrollTop() > fixed.top) {
+                      $('#navContiner').css({"width":"100%","top":"-5px","position": "fixed" , "z-index": "333333"});
+                    }
+                  else{
+                      $('#navContiner').css({"position": " relative"});
+                  }
+                  });
+        })
+    //     $( document ).ready(function() {
+            
+    //         var fixed=$('#navContiner').offset();
+    //         window.addEventListener("resize", ()=>{
+    //             fixed=$('#navContiner').offset();
+    //         });
+    //         $(window).scroll(function() {
+               
+    //                 if ($(document).scrollTop() > fixed.top) {
+    //                   $('#navContiner').css({"width":"100%","top":"-5px","position": "fixed" , "z-index": "333333"});
+    //                 }
+    //               else{
+    //                   $('#navContiner').css({"position": " relative"});
+    //               }
+    //               });
+    // });
         
     }
     render() {
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,22 +31,10 @@
 
     </div>
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script >
-        // alert('kdkksdf')
-        // var fixed=$('#navContiner').offset();
-        // alert(fixed)
-        // $(window).scroll(function() {
-        //     //    const fixed =300;
-        //     // console.log(600)
-
-        //         if ($(document).scrollTop() > fixed.top) {
-        //           //   alert('mohamad')
-        //           $('#navContiner').css({"width":"100%","top":"0","position": "fixed" , "z-index": "333333"});
-        //         }
-        //       else{
-        //           $('#navContiner').css({"position": " relative"});
-        //       }
-        //       });
-    </script>
+    <!-- <script
+			  src="https://code.jquery.com/jquery-3.5.1.min.js"
+			  integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0="
+			  crossorigin="anonymous"></script>-->
+    
 </body>
 </html>
